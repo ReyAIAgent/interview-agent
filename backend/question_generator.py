834d5962d83b3468dbd60469c8464d5f13a61199
ReyAIAgent <rabdolaz@data.syr.edu>
2025-07-21 15:44:28 -0400

    Update question_generator.py

--- backend/question_generator.py
+++ backend/question_generator.py
@@ -4,6 +4,6 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 from agent_interviewer import generate_personalized_question
 
-# New version supports history input
+#  supports history input
 def generate_interview_question(resume_text: str, job_description: str, qa_history: list = None) -> str:
     return generate_personalized_question(resume_text, job_description, history=qa_history)
